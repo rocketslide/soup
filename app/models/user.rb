ac9c4f1a46e014b2a7c39b374a10ac35b5b7cc4c
ecwilhelm <eva@Evas-MacBook.local>
2011-04-16 13:50:38 -0500

    Added validation when adding users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   has_many :visits
+  
+  validates :username, :presence => true, :uniqueness => true
+  
+  validates :email, :presence => true, :uniqueness => true
+  
+  validates :firstname, :presence => true
+  
+  validates :lastname, :presence => true
+  
+  validates :hash_password, :presence => true
+ 
 end
